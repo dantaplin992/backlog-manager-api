b6b37a9e4894bcf622341d8910bc3abc04c26836
Daniel Taplin <98479830+dantaplin992@users.noreply.github.com>
2022-05-16 09:59:10 +0100

    fixed users post route

--- routes/users.js
+++ routes/users.js
@@ -4,6 +4,6 @@ const router = express.Router()
 const UsersController = require("../controllers/users")
 
 router.get("/all", UsersController.All)
-router.get("/new", UsersController.New)
+router.post("/new", UsersController.New)
 
 module.exports = router
