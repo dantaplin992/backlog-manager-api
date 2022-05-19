e93e29cf3fed158acebfa7729b687db75373295f
Daniel Taplin <98479830+dantaplin992@users.noreply.github.com>
2022-05-19 17:27:47 +0100

    news feed route

---
+++ models/news-feed.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose")
+
+const NewsFeedSchema = new mongoose.Schema({
+  userId: String,
+  username: String,
+  action: String,
+  gameTitle: String,
+  timeStamp: {
+    type: Date,
+    default: Date.now
+  }
+})
+
+const NewsFeed = mongoose.model("news-feed", NewsFeedSchema)
+module.exports = NewsFeed
--- public/scripts/socketConnection.js
+++ public/scripts/socketConnection.js
@@ -1,5 +1,36 @@
-function chat() {
+const NewsFeed = require('../../models/news-feed')
 
+function chat(io) {
+  io.on('connection', (socket) => {
+    console.log(`socket connected with id ${socket.id}`)
+    socket.emit('handshake', 'hello client')
+
+    socket.on('addedGame', (params) => {
+      console.log(params)
+      const newFeedItem = new NewsFeed({
+        userId: params.userId,
+        username: params.username,
+        action: "add",
+        gameTitle: params.game.name
+      }).save().then(() => {
+        io.to('newsFeed').emit('refreshFeedItems', params)
+      })
+    })
+
+    socket.on('disconnect', () => {
+      console.log(`socket ${socket.id} disconnected`)
+    })
+
+    socket.on('joinNewsFeed', () => {
+      console.log('joining news feed room')
+      socket.join('newsFeed')
+    })
+
+    socket.on('leaveNewsFeed', () => {
+      console.log('leaving news feed room')
+      socket.leave('newsFeed')
+    })
+  })
 }
 
 module.exports = chat
